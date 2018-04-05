ca238160722969040da5efaef5ca84a3d3fa557e
Richard Kim <richeekim@gmail.com>
2018-04-05 00:16:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,8 +36,9 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
-      if supplies.join(" ") == "BBQ"
-        holiday
+      supplies.each do |x| if x == "BBQ"
+        return holiday
+      end
 
       end
       return holiday
